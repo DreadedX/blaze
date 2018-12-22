31d67a8a8700f2f6fb3d8215e45c40998459bfa4
Tim Huizinga <tim.huizinga@gmail.com>
2018-12-22 03:30:05 +0100

    Fixed build on Windows as command was to long

--- plugin/glsl/flint.lua
+++ plugin/glsl/flint.lua
@@ -1,17 +1,69 @@
 lib "spirv-headers"
 	include "vendor/spirv-headers/include"
 
-lib "spirv-tools"
-	src "**vendor/spirv-tools/source"
+lib "spirv-tools1"
+	src "*vendor/spirv-tools/source/comp"
 	include "vendor/spirv-tools/include"
 	include "vendor/spirv-tools"
 	include "vendor/headers"
 
 	dependency "spirv-headers"
 
+lib "spirv-tools2"
+	src "*vendor/spirv-tools/source/link"
+	include "vendor/spirv-tools/include"
+	include "vendor/spirv-tools"
+	include "vendor/headers"
+
+	dependency "spirv-headers"
+
+lib "spirv-tools3"
+	src "*vendor/spirv-tools/source/opt"
+	include "vendor/spirv-tools/include"
+	include "vendor/spirv-tools"
+	include "vendor/headers"
+
+	dependency "spirv-headers"
+
+lib "spirv-tools4"
+	src "*vendor/spirv-tools/source/reduce"
+	include "vendor/spirv-tools/include"
+	include "vendor/spirv-tools"
+	include "vendor/headers"
+
+	dependency "spirv-headers"
+
+lib "spirv-tools5"
+	src "*vendor/spirv-tools/source/util"
+	include "vendor/spirv-tools/include"
+	include "vendor/spirv-tools"
+	include "vendor/headers"
+
+	dependency "spirv-headers"
+
+lib "spirv-tools6"
+	src "*vendor/spirv-tools/source/val"
+	include "vendor/spirv-tools/include"
+	include "vendor/spirv-tools"
+	include "vendor/headers"
+
+	dependency "spirv-headers"
+
+lib "spirv-tools"
+	src "*vendor/spirv-tools/source"
+	include "vendor/spirv-tools/include"
+	include "vendor/spirv-tools"
+	include "vendor/headers"
+
+	dependency("spirv-tools1", "spirv-tools2", "spirv-tools3", "spirv-tools4", "spirv-tools5", "spirv-tools6")
+
+	dependency "spirv-headers"
+
 lib "libshaderc_util"
 	src "vendor/shaderc/libshaderc_util/src/{compiler,file_finder,io,message,resources,shader_stage,spirv_tools_wrapper,version_profile}.cc"
 	include "vendor/shaderc/libshaderc_util/include"
+
+	include "vendor/glslang"
 
 	dependency "spirv-tools"
 
@@ -20,9 +72,9 @@ lib "glslang"
 	src "**vendor/glslang/glslang/MachineIndependent"
 	src "**vendor/glslang/SPIRV"
 	if config.platform.target == "windows" then
-		src "**vendor/glslang/glslang/OSDependent/Windows"
+		src "vendor/glslang/glslang/OSDependent/Windows/ossource.cpp"
 	else
-		src "**vendor/glslang/glslang/OSDependent/Unix"
+		src "vendor/glslang/glslang/OSDependent/Unix/ossource.cpp"
 	end
 
 	src "**vendor/glslang/OGLCompilersDLL"
@@ -44,4 +96,4 @@ shared "plugin_glsl"
 
 	threads()
 
-	dependency("libshaderc", "flint")
+	dependency("libshaderc", "flint_base")
